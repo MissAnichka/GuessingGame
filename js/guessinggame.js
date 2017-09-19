f0d6f2f14b813a11998aaf1201a57bba264dd2ef
Anna Medyukh <annamedyukh@gmail.com>
2017-09-18 22:42:22 -0400

    added disable buttons after win or lose, changed checkguess to specify what happens on last guess

--- js/guessinggame.js
+++ js/guessinggame.js
@@ -5,22 +5,29 @@ class Game{
       this.winningNumber = generateWinningNumber();
     }
    checkGuess(){
-        if(this.pastGuesses.length > 2 || $('.result').val() === "You Lose."){
-            if(this.pastGuesses.length === 3) this.pastGuesses.push(this.playersGuess);
-            return "You Lose.";
-        } else {
-            if(this.playersGuess === this.winningNumber) return "You Win!";
-            else if(this.pastGuesses.includes(this.playersGuess)) {
+    if(this.pastGuesses.length === 3){
+        if(this.pastGuesses.includes(this.playersGuess)){
             return "You have already guessed that number.";
-            } else {
-                this.pastGuesses.push(this.playersGuess);
-                var diff = this.difference();
-                if(diff < 10) return "You\'re burning up!";
-                if(diff < 25) return "You\'re lukewarm.";
-                if(diff < 50) return "You\'re a bit chilly.";  
-                if(diff < 100) return "You\'re ice cold!";
-            }
+        } else {
+        this.pastGuesses.push(this.playersGuess);
+            if(this.playersGuess === this.winningNumber) return "You Win!";
+            else return "You Lose."
         }
+    }
+    if(this.playersGuess === this.winningNumber){
+        this.pastGuesses.push(this.playersGuess);
+        return "You Win!";
+    }
+    if(this.pastGuesses.includes(this.playersGuess)){
+        return "You have already guessed that number.";
+    } else {
+        this.pastGuesses.push(this.playersGuess);
+        var diff = this.difference();
+        if(diff < 10) return "You\'re burning up!";
+        if(diff < 25) return "You\'re lukewarm.";
+        if(diff < 50) return "You\'re a bit chilly.";  
+        if(diff < 100) return "You\'re ice cold!";
+    }
     }
     playersGuessSubmission(num){
         if(num < 1 || num > 100 || Number.isNaN(num) || typeof(num) !== 'number'){     
@@ -97,8 +104,8 @@ $('#inputsubmit').on('click', function(){
 
 //show 'click reset...' in h3 when player wins/loses:
     if(output === "You Win!" || output === "You Lose."){
+        $('#inputsubmit, .userguess, #hintbutton').prop("disabled",true);
         $('.hintoutput').html('Click Reset to play again!');
-
     }
 
 //put previous guesses into boxes visible to player:
